0aa225c7060f40ed75b017b44f3de4c658fdd236
girishu-ms26 <girishums1999@gmail.com>
2024-01-14 23:42:08 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import {Route,Routes} from "react-router-dom"
 import ProductsList from "./components/ProductsList"
-import Home from "./components/Home";
+import { Link } from "react-router-dom";
 
 function App() {
-    return <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/products" element={<ProductsList />} />
-    </Routes>
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products List</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/products" element={<ProductsList />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App;
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,51 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductService from "../services/ProductService";
 
-class ProductsList extends React.Component {
+// class ProductsList extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            products:[]
-        }
-    }
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             products:[]
+//         }
+//     }
 
-    componentDidMount() {
+//     componentDidMount() {
+//         ProductService.getAllProducts().then((response) => {
+//             this.setState({products: response.data})
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1 className = "text-center">Products List</h1>
+//                 <table className = "table table-striped">
+//                     <thead>
+//                         <tr>
+//                             <td>productName</td>
+//                             <td>price</td>
+//                             <td>quantityAvailable</td>
+//                             <td>modifiedDate</td>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         {
+//                             this.state.products.map(
+//                                 product => 
+//                                 <tr key = {product.productName}>
+//                                     <td>{product.productName}</td>
+//                                     <td>{product.price}</td>
+//                                     <td>{product.quantityAvailable}</td>
+//                                     <td>{product.modifiedDate}</td>
+//                                     <td><button>Update</button></td>
+//                                 </tr>
+//                             )
+//                         }
+//                     </tbody>
+//                 </table>
+//             </div>
+//         )
+//     }
+// }
+
+const ProductsList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
         ProductService.getAllProducts().then((response) => {
-            this.setState({products: response.data})
-        })
-    }
+            setProducts(response.data);
+        });
+    }, []);
 
-    render() {
-        return (
-            <div>
-                <h1 className = "text-center">Products List</h1>
-                <table className = "table table-striped">
-                    <thead>
-                        <tr>
-                            <td>productName</td>
-                            <td>price</td>
-                            <td>quantityAvailable</td>
-                            <td>modifiedDate</td>
+    return (
+        <div>
+            <h1 className="text-center">Products List</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <td>productName</td>
+                        <td>price</td>
+                        <td>quantityAvailable</td>
+                        <td>modifiedDate</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr key={product.productName}>
+                            <td>{product.productName}</td>
+                            <td>{product.price}</td>
+                            <td>{product.quantityAvailable}</td>
+                            <td>{product.modifiedDate}</td>
+                            <td>
+                                <button>Update</button>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.products.map(
-                                product => 
-                                <tr key = {product.productName}>
-                                    <td>{product.productName}</td>
-                                    <td>{product.price}</td>
-                                    <td>{product.quantityAvailable}</td>
-                                    <td>{product.modifiedDate}</td>
-                                    <td><button>Update</button></td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-}
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
 export default ProductsList;
