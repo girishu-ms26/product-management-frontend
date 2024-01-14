b0a7b7782fb6b9d1a17669da48fd08e6f2bd654c
girishu-ms26 <girishums1999@gmail.com>
2024-01-14 18:38:01 +0530

    removed app component

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,11 +1,14 @@
-import axios from "axios";
+import axios, { AxiosHeaders } from "axios";
 
 const PRODUCT_SERVICE_URL = "http://localhost:8081/api/products";
 
 class ProductService {
     getProducts(){
         console.log("Calling the service");
-        var product = axios.get(PRODUCT_SERVICE_URL);
+        const headers = {
+            'apiKey': 'test'
+          }
+        var product = axios.get(PRODUCT_SERVICE_URL,{headers:headers});
         return product;
     }
 }
