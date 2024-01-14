4d6b76ea03140de0701142cd5779c7d3049bbb48
girishu-ms26 <girishums1999@gmail.com>
2024-01-14 23:42:48 +0530

    removed class based component

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,54 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ProductService from "../services/ProductService";
 
-// class ProductsList extends React.Component {
-
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             products:[]
-//         }
-//     }
-
-//     componentDidMount() {
-//         ProductService.getAllProducts().then((response) => {
-//             this.setState({products: response.data})
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1 className = "text-center">Products List</h1>
-//                 <table className = "table table-striped">
-//                     <thead>
-//                         <tr>
-//                             <td>productName</td>
-//                             <td>price</td>
-//                             <td>quantityAvailable</td>
-//                             <td>modifiedDate</td>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {
-//                             this.state.products.map(
-//                                 product => 
-//                                 <tr key = {product.productName}>
-//                                     <td>{product.productName}</td>
-//                                     <td>{product.price}</td>
-//                                     <td>{product.quantityAvailable}</td>
-//                                     <td>{product.modifiedDate}</td>
-//                                     <td><button>Update</button></td>
-//                                 </tr>
-//                             )
-//                         }
-//                     </tbody>
-//                 </table>
-//             </div>
-//         )
-//     }
-// }
-
 const ProductsList = () => {
     const [products, setProducts] = useState([]);
 
